9b4e38f6aafb03ffb387f56294e776da97f69afa
Anže Pečar <apecar@google.com>
2017-09-05 17:10:19 +0100

    Deprecate ggrcdevtest database

--- src/ggrc/settings/testing.py
+++ src/ggrc/settings/testing.py
@@ -1,16 +1,11 @@
 # Copyright (C) 2017 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
-import os
-
 DEBUG = True
 TESTING = True
 LOGIN_DISABLED = False
 PRODUCTION = False
 HOST = '0.0.0.0'
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:root@{}/ggrcdevtest'.format(
-    os.environ.get('GGRC_DATABASE_HOST', 'localhost')
-)
 FULLTEXT_INDEXER = 'ggrc.fulltext.mysql.MysqlIndexer'
 LOGIN_MANAGER = 'ggrc.login.noop'
 # SQLALCHEMY_ECHO = True
